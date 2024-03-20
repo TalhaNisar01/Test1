a43673d34d78eccbe3f640bd69f5c98f368ccd88
TalhaNisar <nisartalha99@gmail.com>
2024-03-20 22:17:49 +0500

    updated

--- Practice/lib/main.dart
+++ Practice/lib/main.dart
@@ -284,3 +284,22 @@ class _DropdownDemoState extends State<DropdownDemo> {
 
 */
 
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Practice",
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+    );
+  }
+}
+
