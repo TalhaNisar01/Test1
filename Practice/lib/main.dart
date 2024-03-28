18b8172ec38cd9fda511196cc3f73f16ca642c85
TalhaNisar <nisartalha99@gmail.com>
2024-03-28 23:22:49 +0500

    updated

--- Practice/lib/main.dart
+++ Practice/lib/main.dart
@@ -1291,5 +1291,40 @@ class MyApp extends StatelessWidget {
 
 */
 
+import 'package:flutter_application_1/SecondPage.dart';
 
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
