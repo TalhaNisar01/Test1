8767c0bf5e2f6594c552f8be82e1433ddde9332a
TalhaNisar <nisartalha99@gmail.com>
2024-05-08 19:30:05 +0500

    updated

--- Practice/lib/main.dart
+++ Practice/lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/SecondPage.dart';
+//import 'package:flutter_application_1/SecondPage.dart';
 //import 'package:flutter_application_1/SecondPage.dart';
 //import 'package:flutter_application_1/SecondPage.dart';
 //import 'package:url_launcher/url_launcher.dart';
